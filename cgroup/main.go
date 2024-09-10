35e78ddf8685b7c52462f3e2b9e2d9f975e3962a
marvol0 <murithimarvin09@gmail.com>
2024-09-10 14:44:53 +0300

    feat(cgroups): set up variables to allocate cpu time and duration of each cgroup

--- cgroup/main.go
+++ cgroup/main.go
@@ -10,3 +10,14 @@ import (
 func pointerInt64(int int64) *int64 {
 	return &int 
 }
+
+func main( {
+	var (
+		quota int64 = 200000
+		period uint = 1000000
+		maj int64 = 8
+		min int64 = 0
+		rate uint64 = 120
+		max int64 = 1000
+	)
+})
