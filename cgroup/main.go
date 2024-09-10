533297150e2c54f29fe34814876deb934e6f7ae6
marvol0 <murithimarvin09@gmail.com>
2024-09-10 15:57:47 +0300

    fix(cgroup): syntax

--- cgroup/main.go
+++ cgroup/main.go
@@ -11,7 +11,7 @@ func pointerInt64(int int64) *int64 {
 	return &int 
 }
 
-func main( {
+func main() {
 	var (
 		quota int64 = 200000
 		period uint = 1000000
@@ -25,5 +25,21 @@ func main( {
 		CPU: &cgroup2.CPU{
 			Max: cgroup2.NewCPUMax(&quota, &period), //CPU time: 200ms per 1 second
 		},
+		Memory: &cgroup2.Memory{
+			Max: pointerInt64(629145600),
+			Swap: pointerInt64(314572800),
+			High: pointerInt64(52488000),
+		},
+		IO: &cgroup2.IO{
+			Max: []cgroup2.Entry{{
+				Major: mj,
+				Minor: min,
+				Type: cgroup2.ReadIOPS,
+				Rate: rate,
+			}},
+		},
+		Pids: &cgroup2.Pids{
+			Max: max,
+		},
 	}
-})
+}
