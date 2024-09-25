328a2137ed5959d1f4c8a97ee2f92baeff630003
marvol0 <murithimarvin09@gmail.com>
2024-09-25 12:43:35 +0300

    feat: create a cgroup manager and start the execute process

--- cgroup/main.go
+++ cgroup/main.go
@@ -8,19 +8,19 @@ import (
 )
 
 func pointerInt64(int int64) *int64 {
-	return &int 
+	return &int
 }
 
 func main() {
 	var (
-		quota int64 = 200000
-		period uint = 1000000
-		maj int64 = 8
-		min int64 = 0
+		quota  int64  = 200000
+		period uint64 = 1000000
+		//weight uint64 = 100
+		maj  int64  = 8
+		min  int64  = 0
 		rate uint64 = 120
 		max int64 = 1000
 	)
-
 	res := cgroup2.Resources{
 		CPU: &cgroup2.CPU{
 			//Weight: &weight, // e.g. (weight in the child cgroup) / (sum of cpu weights in the control groups) => percentage of cpu for this child cgroup processes
@@ -44,5 +44,61 @@ func main() {
 		Pids: &cgroup2.Pids{
 			Max: max, // number of processes allowed - The process number controller is used to allow a cgroup hierarchy to stop any new tasks from being fork()’d or clone()’d after a certain limit is reached.
 		},
+	}
+	
+	// dummy PID of -1 is used for creating a "general slice" to be used as a parent cgroup.
+	// see https://github.com/containerd/cgroups/blob/1df78138f1e1e6ee593db155c6b369466f577651/v2/manager.go#L732-L735
+	// "'-' inside the cgroup name make a child branch => my-cgroup-abc.slice - my.slice/my-cgroup.slice/my-cgroup-abc.slice/<processes>"
+	m, err := cgroup2.NewSystemd("/", "my-cgroup-abc.slice", -1, &res)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	cgType, err := m.GetType()
+	if err != nil {
+		log.Fatalln(err)
+	}
+	log.Println(cgType)
+
+	cmd := exec.Command("stress", "-c", "1", "--timeout", "30")
+	// Start the command
+	if err := cmd.Start(); err != nil {
+		log.Printf("Error starting the command: %v\n", err)
+		return
+	}
+
+	// Retrieve the PID of the started process (+1 because stress command internally spawns another child process depending upon how many CPUs you want to stress (one process per stressed CPU))
+	pid := cmd.Process.Pid + 1
+	log.Printf("PID of the spawned process: %d\n", pid)
+
+
+	if err := m.AddProc(uint64(pid)); err != nil {
+		log.Fatalln(err)
+	}
+
+	procs, err := m.Procs(false)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	log.Printf("List of processes inside this cgroup: %v", procs)
+
+	log.Println("Freezing Process")
+	if err := m.Freeze(); err != nil {
+		log.Fatalln(err)
+	}
+	time.Sleep(time.Second * 15)
+
+	log.Println("Thawing Process")
+	if err := m.Thaw(); err != nil {
+		log.Fatalln(err)
+	}
+
+	// Wait for the command to finish
+	if err := cmd.Wait(); err != nil {
+		log.Printf("Error waiting for the command to finish: %v\n", err)
+		return
+	}
+	err = m.DeleteSystemd()
+	if err != nil {
+		log.Fatalln(err)
 	}
 }
