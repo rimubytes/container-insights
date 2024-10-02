2b76b56b406f5af8fb5ebf90eef4045c5b0c0838
marvol0 <murithimarvin09@gmail.com>
2024-10-02 09:50:41 +0300

    refactor: improve cgroup management code structure and readability

--- cgroup/main.go
+++ cgroup/main.go
@@ -1,104 +1,145 @@
+// Package main provides a program to demonstrate cgroup management using the cgroups/v3 library.
 package main
 
 import (
 	"log"
-	"time"
 	"os/exec"
+	"time"
+
 	"github.com/containerd/cgroups/v3/cgroup2"
 )
 
-func pointerInt64(int int64) *int64 {
-	return &int
+// pointerInt64 returns a pointer to the given int64 value.
+// This is useful for cgroup configurations that require pointers.
+func pointerInt64(i int64) *int64 {
+	return &i
+}
+
+// setupCgroupResources creates and returns a cgroup2.Resources struct
+// with all the resource limits configured.
+func setupCgroupResources() cgroup2.Resources {
+	return cgroup2.Resources{
+		CPU:    setupCPUResources(),
+		Memory: setupMemoryResources(),
+		IO:     setupIOResources(),
+		Pids:   setupPidsResources(),
+	}
+}
+
+// setupCPUResources configures and returns CPU-specific resource limits.
+func setupCPUResources() *cgroup2.CPU {
+	quota := int64(200000)
+	period := uint64(1000000)
+	return &cgroup2.CPU{
+		// Max CPU usage: 200ms per 1000ms (20% of CPU)
+		Max: cgroup2.NewCPUMax(&quota, &period),
+	}
+}
+
+// setupMemoryResources configures and returns memory-specific resource limits.
+func setupMemoryResources() *cgroup2.Memory {
+	return &cgroup2.Memory{
+		Max:  pointerInt64(629145600), // ~629MB max memory usage
+		Swap: pointerInt64(314572800), // ~300MB max swap usage
+		High: pointerInt64(524288000), // ~500MB memory throttle limit
+	}
+}
+
+// setupIOResources configures and returns I/O-specific resource limits.
+func setupIOResources() *cgroup2.IO {
+	return &cgroup2.IO{
+		Max: []cgroup2.Entry{{
+			Major: 8,
+			Minor: 0,
+			Type:  cgroup2.ReadIOPS,
+			Rate:  120, // 120 read operations per second
+		}},
+	}
+}
+
+// setupPidsResources configures and returns process ID limits.
+func setupPidsResources() *cgroup2.Pids {
+	return &cgroup2.Pids{
+		Max: 1000, // Maximum number of processes
+	}
+}
+
+// createCgroup creates a new cgroup with the specified resources.
+func createCgroup(res *cgroup2.Resources) (*cgroup2.Manager, error) {
+	return cgroup2.NewSystemd("/", "my-cgroup-abc.slice", -1, res)
+}
+
+// runStressCommand starts a stress command to simulate CPU load.
+func runStressCommand() (*exec.Cmd, error) {
+	cmd := exec.Command("stress", "-c", "1", "--timeout", "30")
+	err := cmd.Start()
+	if err != nil {
+		return nil, err
+	}
+	return cmd, nil
 }
 
 func main() {
-	var (
-		quota  int64  = 200000
-		period uint64 = 1000000
-		//weight uint64 = 100
-		maj  int64  = 8
-		min  int64  = 0
-		rate uint64 = 120
-		max int64 = 1000
-	)
-	res := cgroup2.Resources{
-		CPU: &cgroup2.CPU{
-			//Weight: &weight, // e.g. (weight in the child cgroup) / (sum of cpu weights in the control groups) => percentage of cpu for this child cgroup processes
-			Max:    cgroup2.NewCPUMax(&quota, &period), // e.g. 200000 1000000 meaning processes inside this cgroup can (together) run on the CPU for only 0.2 sec every 1 second
-			//Cpus:   "0", // This limits on which CPU cores can the processes inside this cgroup run (NOTE: Also "Mems" needs to be set: https://github.com/containerd/cgroups/blob/fa6f6841ed3d57355acadbc06f1d7ed4d91ac4f7/cgroup2/manager.go#L97!)
-			//Mems:   "0", // Memory Node” refers to an on-line node that contains memory. 
-		},
-		Memory: &cgroup2.Memory{
-			Max:  pointerInt64(629145600), // ~629MB // If a cgroup's memory usage reaches this limit and can't be reduced, the system OOM killer is invoked on the cgroup. 
-			Swap: pointerInt64(314572800), // Swap usage in bytes
-			High: pointerInt64(524288000), // memory usage throttle limit. If a cgroup's memory use goes over the high boundary specified here, the cgroup’s processes are throttled and put under heavy reclaim pressure. The default is max, meaning there is no limit.
-		},
-		IO: &cgroup2.IO{
-			Max: []cgroup2.Entry{{
-				Major: maj, 
-				Minor: min, 
-				Type: cgroup2.ReadIOPS, // Limit I/O Read Operations per second for a block device identified as (major, minor) - e.g. "ls -l /dev/sda*"
-				Rate: rate, // number of (read) operations per second
-			}},
-		},
-		Pids: &cgroup2.Pids{
-			Max: max, // number of processes allowed - The process number controller is used to allow a cgroup hierarchy to stop any new tasks from being fork()’d or clone()’d after a certain limit is reached.
-		},
-	}
+	// Set up cgroup resources
+	res := setupCgroupResources()
 	
-	// dummy PID of -1 is used for creating a "general slice" to be used as a parent cgroup.
-	// see https://github.com/containerd/cgroups/blob/1df78138f1e1e6ee593db155c6b369466f577651/v2/manager.go#L732-L735
-	// "'-' inside the cgroup name make a child branch => my-cgroup-abc.slice - my.slice/my-cgroup.slice/my-cgroup-abc.slice/<processes>"
-	m, err := cgroup2.NewSystemd("/", "my-cgroup-abc.slice", -1, &res)
+	// Create the cgroup
+	m, err := createCgroup(&res)
 	if err != nil {
-		log.Fatalln(err)
+		log.Fatalf("Error creating cgroup: %v\n", err)
 	}
+
+	// Get and log the cgroup type
 	cgType, err := m.GetType()
 	if err != nil {
-		log.Fatalln(err)
+		log.Fatalf("Error getting cgroup type: %v\n", err)
 	}
-	log.Println(cgType)
+	log.Println("Cgroup type:", cgType)
 
-	cmd := exec.Command("stress", "-c", "1", "--timeout", "30")
-	// Start the command
-	if err := cmd.Start(); err != nil {
-		log.Printf("Error starting the command: %v\n", err)
-		return
+	// Run the stress command
+	cmd, err := runStressCommand()
+	if err != nil {
+		log.Fatalf("Error starting stress command: %v\n", err)
 	}
 
-	// Retrieve the PID of the started process (+1 because stress command internally spawns another child process depending upon how many CPUs you want to stress (one process per stressed CPU))
+	// Get the PID of the stress command (adding 1 because stress spawns a child process)
 	pid := cmd.Process.Pid + 1
 	log.Printf("PID of the spawned process: %d\n", pid)
 
-
+	// Add the process to the cgroup
 	if err := m.AddProc(uint64(pid)); err != nil {
-		log.Fatalln(err)
+		log.Fatalf("Error adding process to cgroup: %v\n", err)
 	}
 
+	// List processes in the cgroup
 	procs, err := m.Procs(false)
 	if err != nil {
-		log.Fatalln(err)
+		log.Fatalf("Error getting processes in cgroup: %v\n", err)
 	}
 	log.Printf("List of processes inside this cgroup: %v", procs)
 
+	// Freeze the cgroup
 	log.Println("Freezing Process")
 	if err := m.Freeze(); err != nil {
-		log.Fatalln(err)
+		log.Fatalf("Error freezing process: %v\n", err)
 	}
+
+	// Wait for 15 seconds
 	time.Sleep(time.Second * 15)
 
+	// Thaw the cgroup
 	log.Println("Thawing Process")
 	if err := m.Thaw(); err != nil {
-		log.Fatalln(err)
+		log.Fatalf("Error thawing process: %v\n", err)
 	}
 
-	// Wait for the command to finish
+	// Wait for the stress command to finish
 	if err := cmd.Wait(); err != nil {
 		log.Printf("Error waiting for the command to finish: %v\n", err)
-		return
 	}
-	err = m.DeleteSystemd()
-	if err != nil {
-		log.Fatalln(err)
+
+	// Clean up: delete the cgroup
+	if err := m.DeleteSystemd(); err != nil {
+		log.Fatalf("Error deleting cgroup: %v\n", err)
 	}
 }
