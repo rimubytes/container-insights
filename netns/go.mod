c8e0977b4227ff25ade2def212a83b161e04b875
marvol0 <murithimarvin09@gmail.com>
2024-10-07 15:45:46 +0300

    feat: add getNamespaceInfo function
    - Extract namespace info retrival logic into seperate function
    - Improve error handling for namespace info retrieval
    - update main function to use new getNamespaceInfo function

--- netns/go.mod
+++ netns/go.mod
@@ -2,7 +2,4 @@ module netns
 
 go 1.22.2
 
-require (
-	github.com/containernetworking/plugins v1.4.1
-	golang.org/x/sys v0.20.0
-)
+require golang.org/x/sys v0.20.0
--- netns/main.go
+++ netns/main.go
@@ -1,3 +1,4 @@
+// Package main provides functionality for managing network namespaces in Linux.
 package main
 
 import (
@@ -5,8 +6,9 @@ import (
 	"log"
 	"os"
 	"os/exec"
-	"strconv"
+	//"strconv"
 	"syscall"
+
 	"golang.org/x/sys/unix"
 )
 
@@ -14,18 +16,17 @@ const (
 	netNsBasePath = "/tmp/net-ns"
 )
 
-// getNetNsPath returns the base path for network files
+// getNetNsPath returns the base path for network namespace files.
 func getNetNsPath() string {
 	return netNsBasePath
 }
 
-// createDirsIfDontExist creates directories if they dont already exist
-// It takes a slice of directory paths and returns an error if any directory creation fails
-
+// createDirsIfDontExist creates directories if they don't already exist.
+// It takes a slice of directory paths and returns an error if any directory creation fails.
 func createDirsIfDontExist(dirs []string) error {
 	for _, dir := range dirs {
 		if _, err := os.Stat(dir); os.IsNotExist(err) {
-			if err := os.MkdirAll(dir, 0755); err != nil {
+			if err = os.MkdirAll(dir, 0755); err != nil {
 				log.Printf("Error creating directory: %v\n", err)
 				return err
 			}
@@ -43,7 +44,7 @@ func createDirsIfDontExist(dirs []string) error {
 // 5. Bind mounts the new namespace to a file
 // 6. Sets the process back to the original namespace
 func setupNewNetworkNamespace(processID int) {
-	if _, err := createDirsIfDontExist([]string{gettNetNsPath()}); err != nil {
+	if err := createDirsIfDontExist([]string{getNetNsPath()}); err != nil {
 		log.Fatalf("Failed to create directories: %v\n", err)
 	}
 
@@ -66,14 +67,14 @@ func setupNewNetworkNamespace(processID int) {
 		log.Fatalf("Mount system call failed: %v\n", err)
 	}
 
-	if err := unix.Setns(fd,syscall.CLONE_NEWNET); err != nil {
+	if err := unix.Setns(fd, syscall.CLONE_NEWNET); err != nil {
 		log.Fatalf("Setns system call failed: %v\n", err)
 	}
 }
 
-// jointContainerNetworkNamespace sets the current process's network namespace to the one
-// specified by the given process ID
-func jointContainerNetworkNamespace(processID int) error {
+// joinContainerNetworkNamespace sets the current process's network namespace to the one
+// specified by the given process ID.
+func joinContainerNetworkNamespace(processID int) error {
 	nsMount := fmt.Sprintf("%s/%d", getNetNsPath(), processID)
 	fd, err := unix.Open(nsMount, unix.O_RDONLY, 0)
 	if err != nil {
@@ -83,11 +84,32 @@ func jointContainerNetworkNamespace(processID int) error {
 	defer unix.Close(fd)
 
 	if err := unix.Setns(fd, unix.CLONE_NEWNET); err != nil {
-		lod.Printf("Setns system call failed: %v\n", err)
+		log.Printf("Setns system call failed: %v\n", err)
 		return err
 	}
 	return nil
 }
 
+// getNamespaceInfo retrieves and logs the current network namespace information for a given process ID.
+func getNamespaceInfo(processID int) {
+	path := fmt.Sprintf("/proc/%d/ns/net", processID)
+	out, err := exec.Command("readlink", path).Output()
+	if err != nil {
+		log.Fatalf("Error reading namespace file: %v\n", err)
+	}
+	log.Printf("Process is now in the Namespace: %s", string(out))
+}
 
+func main() {
+	processID := os.Getpid()
+	log.Printf("Process ID: %d\n", processID)
 
+	getNamespaceInfo(processID)
+
+	setupNewNetworkNamespace(processID)
+	if err := joinContainerNetworkNamespace(processID); err != nil {
+		log.Fatalf("Failed to join container network namespace: %v\n", err)
+	}
+
+	getNamespaceInfo(processID)
+}
